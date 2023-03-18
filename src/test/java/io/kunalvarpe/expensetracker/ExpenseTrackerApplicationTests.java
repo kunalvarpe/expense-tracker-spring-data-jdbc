cf81a0bf8ad842397d8f89ab3ebfd17745c80418
Kunal Varpe <kunalvarpe@outlook.com>
2023-03-19 00:56:53 +0530

    Initial project setup

---
+++ src/test/java/io/kunalvarpe/expensetracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kunalvarpe.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
