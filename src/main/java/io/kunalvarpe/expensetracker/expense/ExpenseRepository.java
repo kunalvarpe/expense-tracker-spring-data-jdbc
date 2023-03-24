2c74dbc4d6c2d1ebb0b3a5b32685d6930f262c00
Kunal Varpe <kunalvarpe@outlook.com>
2023-03-24 12:52:44 +0530

    Fix integration test with test containers

--- src/main/java/io/kunalvarpe/expensetracker/expense/ExpenseRepository.java
+++ src/main/java/io/kunalvarpe/expensetracker/expense/ExpenseRepository.java
@@ -1,11 +1,19 @@
 package io.kunalvarpe.expensetracker.expense;
 
 import java.util.Collection;
+import java.util.List;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ExpenseRepository extends Repository<Expense, Integer> {
 
 	Collection<Expense> findByUserId(Integer userId);
+
+	@Transactional(readOnly = true)
+	@Query("select * from expenses order by id desc")
+	List<Expense> findAll() throws DataAccessException;
 
 }
